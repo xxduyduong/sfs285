b8f3bc8fae9cc74c0d26f0f765d3a40360badfc1
ToanTran <toan@9fury.com>
2015-01-15 09:26:37 +0700

    edit run

--- sfs2x-standalone.command
+++
@@ -1,2 +0,0 @@
-cd "`dirname "$0"`"
-./sfs2x-standalone
--- sfs2x.sh
+++ sfs2x.sh
@@ -2,11 +2,10 @@
 # Use -Xms512M -Xmx512M to start with 512MB of heap memory. Set size according to your needs.
 # Use -XX:+CMSClassUnloadingEnabled -XX:+CMSPermGenSweepingEnabled for PermGen GC
 
-JRE_FOLDER="../jre/bin"
+#JRE_FOLDER="../jre/bin"
 JAVA_CMD="java"
-if [ -d "$JRE_FOLDER" ]; then
-    JAVA_CMD="../jre/bin/java"
-fi
+#    JAVA_CMD="../jre/bin/java"
+#fi
 
-CPATH="./:lib/*:lib/jetty/lib/*:extensions/__lib__/*"
-${JAVA_CMD} -cp ${CPATH} -Dfile.encoding=UTF-8 com.smartfoxserver.v2.Main $1 $2
+CPATH="./:lib/*:lib/Jetty/*:lib/Lib/*:extensions/__lib__/*"
+${JAVA_CMD} -server ${CPATH} -Dfile.encoding=UTF-8 com.smartfoxserver.v2.Main $1 $2
